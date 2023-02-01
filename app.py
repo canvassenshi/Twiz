3a318a8502b322e0b5a80300d792827b66079eb5
Ravi <68025703+canvassenshi@users.noreply.github.com>
2023-02-01 12:36:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, url_for, render_template, request, redirect
+from flask_socketio import SocketIO, send
+
+
+app = Flask(__name__)
+
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+
+@socketio.on("message")
+def sendMessage(message):
+    send(message, broadcast=True)
+
+@app.route("/")
+def signUp():
+    return render_template("getuser.html")
+
+@app.route("/index")
+def message():
+    username = request.args.get("username")
+    if username:
+        return render_template("index.html", username = username)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
